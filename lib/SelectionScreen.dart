08710a52f2df082706d11471c6b6105b89b36d53
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2020-05-09 23:52:56 +0300

    code cleanup

--- lib/SelectionScreen.dart
+++ lib/SelectionScreen.dart
@@ -5,34 +5,26 @@ class SelectionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pick an option'),
+        title: Text('Choose chapter'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: () {
-                  // Close the screen and return "Yep!" as the result.
-                  Navigator.pop(context, 'Chapter 1');
-                },
-                child: Text('Chapter 1'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: RaisedButton(
-                onPressed: () {
-                  // Close the screen and return "Nope!" as the result.
-                  Navigator.pop(context, 'Chapter 2');
-                },
-                child: Text('Chapter 2'),
-              ),
-            )
-          ],
-        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              // Close the screen and return "Yep!" as the result.
+              Navigator.pop(context, 'Chapter 1');
+            },
+            child: Text('Chapter 1'),
+          ),
+          RaisedButton(
+            onPressed: () {
+              // Close the screen and return "Nope!" as the result.
+              Navigator.pop(context, 'Chapter 2');
+            },
+            child: Text('Chapter 2'),
+          ),
+        ],
       ),
     );
   }
