c8b8ef4309b09807f5f95cd73161f528bde61442
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2020-05-09 23:21:29 +0300

    Made getting chapter

--- lib/Chapter.dart
+++ lib/Chapter.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'SelectionScreen.dart';
+
 class Chapter extends StatefulWidget {
   @override
   _ChapterState createState() => _ChapterState();
 }
 
 class _ChapterState extends State<Chapter> {
+  String value;
+
+  @override
+  void initState() => value = 'Starting Chapter';
+
+  Future<String> nextRoute(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+    setState(() {
+      if (result) value = result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello'),
-      ),
+          title: Text(value),
+          leading: GestureDetector(
+            onTap: () => nextRoute(context),
+            child: Icon(Icons.select_all),
+          )),
       body: Container(
         padding: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
         child: Center(
             child: ListView(
-          children: List<int>.generate(100, (i) => i + 1)
+          children: List<int>.generate(40, (i) => i + 1)
               .map((v) => Verse(
                   v, 'ܡܢ ܟܠܗܝܢ ܗܠܝܢ ܣܓܼܝܬ ܚܟܡܬܐ ܘܥܼܫܢܬ ܗܝܡܢܘܬܐ܂ ܡܢ ܠܩܘܕܡܝܢ '))
               .toList(),
---
+++ lib/SelectionScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Close the screen and return "Yep!" as the result.
+                  Navigator.pop(context, 'Chapter 1');
+                },
+                child: Text('Chapter 1'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Close the screen and return "Nope!" as the result.
+                  Navigator.pop(context, 'Chapter 2');
+                },
+                child: Text('Chapter 2'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
